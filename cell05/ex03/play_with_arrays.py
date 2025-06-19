813c8c0f548135f582623cd91bb799c6db555d9d
Yanapat15060 <cupong15060@gmail.com>
2025-06-19 03:34:26 +0000

    original_array

--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -1,8 +1,4 @@
-#!/usr/bin/env python3
-
 original_array = [2, 8, 9, 48, 8, 22, -12, 2]
-new_array = [x + 2 for x in original_array if x > 5]
-unique_values = set(new_array)
-
 print(original_array)
-print(unique_values)
+new_set = {x + 2 for x in original_array if x > 5}
+print(new_set)
