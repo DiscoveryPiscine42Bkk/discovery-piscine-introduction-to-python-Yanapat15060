682c1a67c49474c9919f3fc5b9abe7c4efc1ec8c
Yanapat15060 <cupong15060@gmail.com>
2025-06-19 03:35:03 +0000

    import sys

--- cell05/ex04/parameters.py
+++ cell05/ex04/parameters.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
 import sys
-
-count = len(sys.argv) - 1
-print(f"Number of parameters: {count}.")
+num_parameters = len(sys.argv) - 1 
+print(f"Number of parameters: {num_parameters}.")
