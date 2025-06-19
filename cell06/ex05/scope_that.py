072dc5a75d256d3155293d3eedb4fe1eddde7159
Yanapat15060 <cupong15060@gmail.com>
2025-06-19 06:42:45 +0000

    cell6

---
+++ cell06/ex05/scope_that.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def add_one(x):
+    x += 1
+
+num = 42
+print(num)
+add_one(num)
+print(num)
+Cell06🟢
+ex00 python3 hello_all.py
+ex01 python3 upcase_it.py
+ex02 python3 downcase_all.py
+ex03 python3 greetings_for_all.py
+ex04 python3 methods_everywhere.py
+ex05 python3 scope_that.py
